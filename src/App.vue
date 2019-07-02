649b0dc08988a01b8f91b624f8dfb49912ca8abd
sarahheimerdinger <sarah21hei@gmail.com>
2019-07-02 12:33:45 -0500

    adds bootstrap and navbar

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,17 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
+    <nav class="navbar navbar-expand-lg navbar-light bg-light">
+      <a class="navbar-brand" href="/">Student Portal</a>
+      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+        <div class="navbar-nav">
+          <router-link to="/resumes/1" class="nav-item nav-link">View Resume</router-link>
+          <router-link to="/resumes/1/edit" class="nav-item nav-link">Edit Resume</router-link> 
+        </div>
+      </div>
+    </nav>
     <router-view/>
   </div>
 </template>
