4d3503c3a247e137dd5b4d56ecd5c05146cb89fd
Mark Harrison <mhworld11@icloud.com>
2019-07-02 10:50:39 -0700

    creates edit education form

--- src/views/Resumes/Edit.vue
+++ src/views/Resumes/Edit.vue
@@ -39,6 +39,44 @@
       </form><br>
     </div>
 
+    
+    <h2 class="text-center">Edit Education</h2>
+
+    <div v-for="education in educations">
+      <form v-on:submit.prevent="submit()">
+        <div class="form-row">
+          <div class="form-group col-md-6">
+            <label for="start_date">Start Date:</label>
+            <input type="string" class="form-control" id="start_date" placeholder="7/14/2015" v-model="education.start_date">
+          </div> 
+        </div>
+        <div class="form-row">
+          <div class="form-group col-md-6">
+            <label for="end_date">End Date:</label>
+            <input type="string" class="form-control" id="end_date" placeholder="05/22/2019" v-model="education.end_date">
+          </div> 
+        </div>
+        <div class="form-row">
+          <div class="form-group col-md-6">
+            <label for="degree">Degree:</label>
+            <input type="string" class="form-control" id="degree" placeholder="bachelor" v-model="education.degree">
+          </div> 
+        </div>
+        <div class="form-row">
+          <div class="form-group col-md-6">
+            <label for="university_name">University Name:</label>
+            <input type="string" class="form-control" id="university_name" placeholder="Your school name" v-model="education.university_name">
+          </div> 
+        </div>
+        <div class="form-row">
+          <div class="form-group col-md-6">
+            <label for="details">Details:</label>
+            <input type="text" class="form-control" id="details" placeholder="Please input educational details" v-model="education.details">
+          </div> 
+        </div>
+      </form>
+    </div>
+
   </div>
 </template>
 
@@ -62,8 +100,24 @@ export default {
           company_name: "company name 2",
           details: "details 2 details 2 details 2 details 2 details 2 details 2 details 2 "},
       ],
+      educations: [
+        {
+          start_date: "01-01-2019",
+          end_date: "06-01-2019",
+          degree: "bachelors 1",
+          university_name: "university name 1",
+          details: "details 1 details 1 details 1 details 1 details 1 details 1 details 1 "},
+        {
+          start_date: "02-01-2016",
+          end_date: "06-01-2018",
+          degree: "bachelors 2",
+          university_name: "university name 2",
+          details: "details 2 details 2 details 2 details 2 details 2 details 2 details 2 "},
+      ],
     };
+
   },
+  
   created: function() {
   },
   methods: {
